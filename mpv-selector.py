52acf64f9451ce8d5e89d5cf90c249ecf8b7f181
Blair Edwards <blaired1@yahoo.co.uk>
2019-06-19 16:35:07 +0100

    Added subtitles support - automatically embeds them in the video provided FFMPEG is installed. Cleaned things up a bit.

--- mpv-selector.py
+++ mpv-selector.py
@@ -8,35 +8,37 @@
 #"C:\Program Files\MPV\mpv.exe" --ytdl-format="bestvideo[height<=?1080][ext=?webm]+bestaudio/best" --cache=1048576
 
 from subprocess import Popen
+from os import environ
+environ["HOME"] = "/Users/Blair"
 
 mpvPath = "/Program Files/MPV/"
-mpvExe = "mpv.exe"
-ytdlExe = "youtube-dl.exe"
-dlLoc = ' -o "C:/Users/Blair/Videos/%(title)s.%(ext)s"'
+commonCmds = "--netrc --all-subs --embed-subs "
+dlCmds = 'youtube-dl.exe -o "C:/Users/Blair/Videos/%(title)s.%(ext)s" --format="'
+noDlCmds = 'mpv.exe --ytdl-format="'
 
 watchOrDl = input ("(w)atch or (d)ownload?  ")
 print ("Good qualities include:  a; audio; 720; 1080; 1440; 2160.")
-theQuality = input ("Which quality would you like?  ")
+userQuality = input ("Which quality would you like?  ")
 theUrl = input ("Plese drop your URL here:  ")
 
-if theQuality == "audio" or theQuality == "a":
-	theCmd = 'bestaudio/best" '
-elif theQuality == "":
-	theCmd = 'best" --cache=1048576 '
+if userQuality == "audio" or userQuality == "a":
+	theQuality = 'bestaudio/best" '
+elif userQuality == "":
+	theQuality = 'best" --cache=1048576 '
 else:
-	theCmd = "bestvideo[height<=?" + theQuality + '][ext=?webm]+bestaudio/best" --cache=1048576 '
+	theQuality = "bestvideo[height<=?" + userQuality + '][ext=?webm]+bestaudio/best" --cache=1048576 '
 
 if watchOrDl == "w":
-	theCmdPre = ' --ytdl-format="'
-	theWholeCmd = mpvPath + mpvExe + theCmdPre + theCmd + theUrl
+	specCmds = noDlCmds
 	
 elif watchOrDl == "d":
-	theCmdPre = ' --format="'
-	theWholeCmd = mpvPath + ytdlExe + dlLoc + theCmdPre + theCmd + theUrl
+	specCmds = dlCmds
 
 else:
 	print ("Error")
 	exit (0)
 
 print ("Sure thing, boss.")
+theWholeCmd = mpvPath + specCmds + theQuality + commonCmds + theUrl
+print (theWholeCmd)
 Popen (theWholeCmd)
